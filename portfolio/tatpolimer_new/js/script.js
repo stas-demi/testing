aec19a7138fbed5930481befe44ef3adb8c9f30b
stas-demi <59840104+stas-demi@users.noreply.github.com>
2022-02-20 10:54:52 +0200

    Add files via upload

---
+++ portfolio/tatpolimer_new/js/script.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    items:4,
+    margin:10,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
